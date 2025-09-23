01bcc75cdf6ddc9bc3ef90ec84933125ab3f1274
Jack Behnke <300068570@fvtc.edu>
2025-09-22 19:11:22 -0500

    first commit

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,8 @@
 <?php
-$isHome = $_SERVER['REQUEST_URI'] == '/';
-$isLoops = $_SERVER['REQUEST_URI'] == '/loops/';
-$isCountdown = $_SERVER['REQUEST_URI'] == '/countdown/'
+$isHome = $_SERVER['REQUEST_URI'] == '/' ? 'selected' : '';
+$isLoops = $_SERVER['REQUEST_URI'] == '/loops/' ? 'selected' : '';
+$isCountdown = $_SERVER['REQUEST_URI'] == '/countdown/' ? 'selected' : '';
+$isMagic8Ball = $_SERVER['REQUEST_URI'] == '/magic-8ball/' ? 'selected' : '';
 
 ?><nav>
 
@@ -10,5 +11,8 @@ $isCountdown = $_SERVER['REQUEST_URI'] == '/countdown/'
         <li class="<?=$isHome?>"><a href="/">Home</a></li>
         <li class="<?=$isLoops?>"><a href="/loops">Loops</a></li>
         <li class="<?=$isCountdown?>"><a href="/countdown">Countdown</a></li>
+        <li class="<?=$isMagic8Ball?>">
+            <a href="/magic-8ball">Magic 8 Ball</a>
+        </li>
     </ul>
 </nav>
---
+++ tests/recieve.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_POST['firstName'])) echo "got an fn";
+    if(isset($_POST['lastName'])) echo "got a ln";
+?><!doctype html>
+<html>
+<head>
+    <title>Showing Data</title>
+</head>
+<body>
+
+
+
+</body>
+</html>
